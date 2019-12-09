20664d0381ae45d7cf9e053561eb5cab50c78831
vincentcaptain <sjliang@stanford.edu>
2019-12-08 21:42:17 -0800

    gs.py

--- Ehrenfest_water/dissociation.py
+++ Ehrenfest_water/dissociation.py
@@ -8,23 +8,41 @@ from ase.parallel import parprint
 name = 'H2O_laser'
 
 # Ehrenfest simulation parameters
-timestep = 40.0					# 10 attoseconds
+timestep = 40.0					# 40 attoseconds
 ndiv = 10						# dump traj every 10 timesteps
 niter = 500						# up to 500 timesteps = 20 fs
 
 # TDDFT calculator with external potential from intense harmonic laser field
 # Laser parameters:
-# -- field intensity: E = sqrt(377*Pd) = 6.14E9 V/m = 0.0119 au
+# -- field intensity: E = sqrt(754*Pd) = 8.68E9 V/m = 0.0169 au
 # -- field freq: w = 0.057 au
-# -- switch on time: 40 attoseconds = 1.654 aut
+# -- switch on time: 20000 attoseconds = 827 aut
 tdcalc = TDDFT(name + '_gs.gpw', 
 			   txt = name + '_td.txt', 
 			   propagator = "EFSICN", 
 			   solver = "BiCGStab", 
-			   td_potential = CWField(0.0119, 0.057, 1.654))
+			   td_potential = CWField(0.0169, 0.057, 827))
 
 # New object for velocity verlet
 ehrenfest = EhrenfestVelocityVerlet(tdcalc)
 
+
 # Another object to save the dynamics
 traj = Trajectory(name + '_td.traj', 'w', tdcalc.get_atoms())
+
+for i in range(1, niter + 1):
+	ehrenfest.propagate(timestep)
+
+
+	# Every ndiv timesteps, save the trajectory
+	if i % ndiv == 0:
+		# energy, forces, velocities to save.
+		print(i)
+		epot = tdcalc.get_td_energy() * Hartree
+		F_av = ehrenfest.F * Hartree / Bohr
+		v_av = ehrenfest.v * Bohr / AUT
+		atoms = tdcalc.atoms.copy()
+		atoms.set_velocities(v_av)
+		traj.write(atoms, energy = epot, forces = F_av)
+
+traj.close()
